b05d9214e623e8e59bd17e4bcc0c1f514d91727c
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-10-24 17:50:19 -0300

    feat: Inicío dos estudos

---
+++ src/main/java/dev/luis/goes/spring/actuator/studies/SpringBootActuatorStudiesApplication.java
@@ -0,0 +1,13 @@
+package dev.luis.goes.spring.actuator.studies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorStudiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActuatorStudiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/luis/goes/spring/actuator/studies/SpringBootActuatorStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luis.goes.spring.actuator.studies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorStudiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
