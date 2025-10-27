f4aa18a0c6a09bfe13ecc3d76b1b46c8b9d52d62
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-10-27 17:41:34 -0300

    feat: Enable Spring Data web support with DTO serialization

--- src/main/java/dev/luis/goes/spring/actuator/studies/SpringBootActuatorStudiesApplication.java
+++ src/main/java/dev/luis/goes/spring/actuator/studies/SpringBootActuatorStudiesApplication.java
@@ -2,8 +2,12 @@ package dev.luis.goes.spring.actuator.studies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class SpringBootActuatorStudiesApplication {
 
     public static void main(String[] args) {
